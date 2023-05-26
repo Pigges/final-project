aaf66fb6edff0d9cd6a041e7ba791cbd66e5d8b9
Pigges <contact@pigg.es>
2023-05-26 20:28:32 +0200

    clear search_box when clicking search

--- header.py
+++ header.py
@@ -6,15 +6,16 @@ from tkinter import ttk
 import search_city
 
 # Search results
-def city_search(root, query):
+def city_search(root, search_box):
     """
     Call search_city to make a city search
 
     Keyword arguments:
     root -- the master tkinter frame
-    query -- the search query
+    search_box -- the search_box
     """
-    search_city.show_results(root, query)
+    search_box.delete(0) # Clear search box field
+    search_city.show_results(root, search_box.get())
     return True
 
 # Set up the Header Frame
@@ -30,6 +31,6 @@ def create_header(root):
     ttk.Label(header, text="Lookup weather for").grid(column=0, row=0, padx=10)
     search_box = ttk.Entry(header)
     search_box.grid(column=1, row=0)
-    search = ttk.Button(header, text="Search", command=lambda: city_search(root, search_box.get()))
+    search = ttk.Button(header, text="Search", command=lambda: city_search(root, search_box))
     search.grid(column=2, row=0, padx=10)
     return True
